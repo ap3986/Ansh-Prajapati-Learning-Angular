afb3b539f5e9cf3e50b4201e9487f5331f284947
Ansh Prajapati <W0847429@myscc.ca>
2024-09-26 16:45:11 -0400

    Created Input Properties

--- src/app/cosmetic-list/cosmetic-list.component.ts
+++ src/app/cosmetic-list/cosmetic-list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {CosmeticProject} from "../models/cosmeticProject";
+import {CosmeticListItemComponent} from "../cosmetic-list-item/cosmetic-list-item.component";
 
 @Component({
   selector: 'app-cosmetic-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CosmeticListItemComponent
+  ],
   templateUrl: './cosmetic-list.component.html',
   styleUrl: './cosmetic-list.component.css'
 })
 export class CosmeticListComponent {
+  userList : CosmeticProject[] = [
+    {serialNumber: 100, productName: "Glow Serum", price: 20, color: "Soft Rose", skinType: "Normal Skin", userInformation: "No"},
+    {serialNumber: 200, productName: "Hydration Booster", price: 25, color: "Crystal Clear", skinType: "Dry Skin", userInformation: "No"},
+    {serialNumber: 300, productName: "Mattifying Primer", price: 30, color: "Mint Green", skinType: "Oily Skin", userInformation: "Yes"},
+    {serialNumber: 400, productName: "Youthful Glow Cream", price: 35, color: "Lavender Mist", skinType: "Mature Skin", userInformation: "No"},
+    {serialNumber: 500, productName: "Blissful Blush", price: 18, color: "Peachy Pink", skinType: "All Skin Types", userInformation: "No"},
+    {serialNumber: 600, productName: "Ethereal Eyeshadow Palette", price: 40, color: "Vibrant Mix", skinType: "All Skin Types", userInformation: "Yes"}
 
+  ]
 }
