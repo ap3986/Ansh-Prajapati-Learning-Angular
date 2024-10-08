7a8a2b1d6f8ba1149dfbb683041034e4ae48e451
Ansh Prajapati <W0847429@myscc.ca>
2024-10-08 16:18:58 -0400

    Completed Assignment 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CosmeticProject} from "./Shared/models/cosmeticProject";
 import {NgForOf, NgIf} from "@angular/common";
 import {CosmeticListComponent} from "./cosmetic-list/cosmetic-list.component";
+import {CosmeticListItemComponent} from "./cosmetic-list-item/cosmetic-list-item.component";
+import {CosmeticService} from "./Services/cosmetic.service";
+import {userList} from "./Shared/data/mock-content";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf, CosmeticListComponent],
+  imports: [RouterOutlet, NgForOf, NgIf, CosmeticListComponent, CosmeticListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Aura Allure';
-  userList : CosmeticProject[] = [
-  {serialNumber: 100, productName: "Glow Serum", price: 20, color: "Soft Rose", skinType: "Normal Skin", userInformation: "No"},
-  {serialNumber: 200, productName: "Hydration Booster", price: 25, color: "Crystal Clear", skinType: "Dry Skin", userInformation: "No"},
-  {serialNumber: 300, productName: "Mattifying Primer", price: 30, color: "Mint Green", skinType: "Oily Skin", userInformation: "Yes"},
-  {serialNumber: 400, productName: "Youthful Glow Cream", price: 35, color: "Lavender Mist", skinType: "Mature Skin", userInformation: "No"},
-  {serialNumber: 500, productName: "Blissful Blush", price: 18, color: "Peachy Pink", skinType: "All Skin Types", userInformation: "No"},
-  {serialNumber: 600, productName: "Ethereal Eyeshadow Palette", price: 40, color: "Vibrant Mix", skinType: "All Skin Types", userInformation: "Yes"}
+export class AppComponent implements OnInit{
+  constructor(private cosmeticService: CosmeticService) {
 
-]
+  }
+
+  title = 'Aura Allure';
+
+  cosmetic : CosmeticProject | undefined;
+  ngOnInit(): void {
+    this.cosmeticService.getCosmeticsByserialNumber(100).subscribe({
+      next :(data: CosmeticProject | undefined) => this.cosmetic =data
+    })
+  }
+
 }
