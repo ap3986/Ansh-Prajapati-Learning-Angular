a307c722b235da9761269eb4d0459882e706413e
Ansh Prajapati <W0847429@myscc.ca>
2024-11-04 12:38:35 -0500

    completed step-07

--- src/app/Services/cosmetic.service.ts
+++ src/app/Services/cosmetic.service.ts
@@ -1,41 +1,45 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from "rxjs";
+import {catchError, Observable, of, throwError} from "rxjs";
 import {CosmeticProject} from "../Shared/models/cosmeticProject";
 import {userList} from "../Shared/data/mock-content";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 
 export class CosmeticService {
+  private apiUrl = 'api/cosmetics';
   private cosmetics : CosmeticProject[] = userList;
-  constructor() { }
-  getCosmetics(): Observable<CosmeticProject[]>{
-    return of (this.cosmetics)
+  constructor(private http: HttpClient) { }
+  getCosmetics(): Observable<CosmeticProject[]> {
+    return this.http.get<CosmeticProject[]>(this.apiUrl).pipe(catchError(this.handleError));
   }
 
-  getCosmeticByserialNumber(serialNumber:number): Observable<CosmeticProject | undefined>{
-   return of (this.cosmetics.find(cosmetic => cosmetic.serialNumber === serialNumber));
-  }
+  getCosmeticByserialNumber(serialNumber:number): Observable<CosmeticProject>{
+    return this.http.get<CosmeticProject>(`${this.apiUrl}/${serialNumber}`).pipe(catchError(this.handleError));  }
 
   addCosmetic(cosmetic:CosmeticProject): Observable<CosmeticProject>{
-    this.cosmetics.push(cosmetic)
-    return of (cosmetic)
+    cosmetic.serialNumber = this.generateNewserialNumber();
+    return this.http.post<CosmeticProject>(this.apiUrl, cosmetic).pipe(catchError(this.handleError));
+
   }
 
-  updateCosmetic(updatedCosmetic : CosmeticProject ): Observable<CosmeticProject | undefined >{
-    const index = this.cosmetics.findIndex(cosmetic => cosmetic.serialNumber == updatedCosmetic.serialNumber);
-    if(index !== -1 ){
-      this.cosmetics[index] = updatedCosmetic;
-      return of (updatedCosmetic)
-    }
-    return of (undefined);
+  updateCosmetic(cosmetic: CosmeticProject): Observable<CosmeticProject | undefined> {
+    const url = `${this.apiUrl}/${cosmetic.serialNumber}`;
+    return this.http.put<CosmeticProject>(url, cosmetic).pipe(catchError(this.handleError));
   }
 
-  deleteCosmetic(serialNumber: number): void{
-    this.cosmetics = this.cosmetics.filter(cosmetic => cosmetic.serialNumber !== serialNumber);
+
+  deleteCosmetic(serialNumber: number): Observable<{}> {
+    const url = `${this.apiUrl}/${serialNumber}`;
+    return this.http.delete(url).pipe(catchError(this.handleError));
   }
   generateNewserialNumber(): number {
     return this.cosmetics.length > 0 ? Math.max(...this.cosmetics.map(cosmetic => cosmetic.serialNumber)) + 1 : 1;
   }
 
+  private handleError(error: HttpErrorResponse) {
+    console.error('API error:', error);
+    return throwError(() => new Error('Server error, please try again.'));
+  }
 }
