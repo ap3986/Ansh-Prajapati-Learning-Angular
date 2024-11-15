db5b594f4cd37cd92deedc6d16052299216e0923
Ansh Prajapati <W0847429@myscc.ca>
2024-11-14 19:16:42 -0500

    applied lazy loading

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from "@angular/router";
 import {CosmeticListComponent} from "./app/cosmetic-list/cosmetic-list.component";
-import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
-import {CosmeticListItemComponent} from "./app/cosmetic-list-item/cosmetic-list-item.component";
 
 const routes:Routes = [
   {path:'',redirectTo:'/cosmetics',pathMatch:'full'},
   {path:'cosmetics',component:CosmeticListComponent},
-  {path:'cosmetics/:serialNumber', component:CosmeticListItemComponent},
-  {path:'modify-cosmetic',component:ModifyListItemComponent},
-  {path:'**', component:PageNotFoundComponent},
+  {path:'cosmetics/:serialNumber', loadComponent: () =>
+      import('./app/cosmetic-list-item/cosmetic-list-item.component').then(m => m.CosmeticListItemComponent)},
+  {path:'modify-cosmetic',loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent)},
+  {path:'**',loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)},
 ]
 
 bootstrapApplication(AppComponent, {
